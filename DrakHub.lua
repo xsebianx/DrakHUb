3e2d3257e02b2f8051903559b5ecd767ca94bfaa
Drakath <67465408+xsebianx@users.noreply.github.com>
2025-07-02 14:11:34 -0300

    Update DrakHub.lua

--- DrakHub.lua
+++ DrakHub.lua
@@ -1,16 +1,18 @@
 local HttpService = game:GetService("HttpService")
 local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
-local url = "https://tureplit.repl.co/?hwid=" .. hwid
+local url = "https://46191531-d1f8-4e08-a0ac-5b0e1aa22935-00-296luuro2wrme.picard.replit.dev/?hwid=" .. hwid
 
 local success, resultado = pcall(function()
     return HttpService:GetAsync(url)
 end)
 
 if success and resultado and resultado ~= "no_autorizado" then
-    local menu = loadstring(resultado)
-    if menu then
-        menu()
+    local func, err = loadstring(resultado)
+    if func then
+        func()
+    else
+        warn("Error al cargar el menú: " .. tostring(err))
     end
 else
-    warn("No autorizado")
+    warn("No autorizado para usar el menú")
 end
